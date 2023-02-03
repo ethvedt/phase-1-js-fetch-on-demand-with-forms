436f40dc8b13d522780623e66df92887e7f86d99
Eric Thvedt <ericsthvedt@gmail.com>
2023-02-03 10:50:18 -0800

    everything works

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+  formSetup();
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+function formSetup() {
+    const form = document.querySelector('form');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const input = document.querySelector("input#searchByID");
+
+        fetch(`http://localhost:3000/movies/${input.value}`)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                const title = document.querySelector(`section#movieDetails h4`);
+                const summary = document.querySelector(`section#movieDetails p`);
+                title.textContent = data.title;
+                summary.textContent = data.summary;
+            });
+    });
+};
